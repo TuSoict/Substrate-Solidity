edfeff74c1505e4d2bb9f64aaf9590fec6881f52
CakChyn <trungbaotran1701@gmail.com>
2022-12-04 07:58:30 +0700

    test

---
+++ testing/target/debug/deps/testing-46ad91ac2fda8cd3.d
@@ -0,0 +1,5 @@
+/home/cakchyn/Downloads/Substrate-Solidity/testing/target/debug/deps/testing-46ad91ac2fda8cd3.rmeta: src/main.rs
+
+/home/cakchyn/Downloads/Substrate-Solidity/testing/target/debug/deps/testing-46ad91ac2fda8cd3.d: src/main.rs
+
+src/main.rs:
