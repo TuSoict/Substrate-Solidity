c7a3cd9cc68eae6aa92f8d9388eb417da7620172
CakChyn <trungbaotran1701@gmail.com>
2022-12-10 20:20:56 +0700

    delele old code

--- testing/target/debug/deps/testing-e919d72d7536606f.d
+++
@@ -1,5 +0,0 @@
-/home/cakchyn/Downloads/Substrate-Solidity/testing/target/debug/deps/testing-e919d72d7536606f.rmeta: src/main.rs
-
-/home/cakchyn/Downloads/Substrate-Solidity/testing/target/debug/deps/testing-e919d72d7536606f.d: src/main.rs
-
-src/main.rs:
